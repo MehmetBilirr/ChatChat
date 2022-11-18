09b0cc95b47345612aa9a1a81a0e54784a910c20
MehmetBilirr <mehmetbilir@aol.com>
2022-11-18 23:01:29 +0300

    Initial Commit

---
+++ ChatChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatChat
+//
+//  Created by Mehmet Bilir on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
